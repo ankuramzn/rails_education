d80cd534ef7457ce36827b34073b4f72eb8686bf
Ankur <saksena@amazon.com>
2011-03-14 19:58:10 -0700

    Adding the :mysql_root_password => "your-mysql-root-password",

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,8 @@ role :db,  "ec2-50-16-175-141.compute-1.amazonaws.com", :primary => true # This 
 
 set :rails_env, "production"
 
+
+
 set :ec2onrails_config, {
 
   # Set the server timezone. run "cap -e ec2onrails:server:set_timezone" for
@@ -27,6 +29,8 @@ set :ec2onrails_config, {
   # want to receive mail from the server (e.g. monit alert messages) then
   # remove this.
   :admin_mail_forward_address => "ankur.saksena@gmail.com",
+
+  :mysql_root_password => "",
 
   # Set this if you want SSL to be enabled on the web server. The SSL cert
   # and key files need to exist on the server, The cert file should be in
